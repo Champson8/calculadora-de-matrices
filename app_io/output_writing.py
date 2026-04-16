cbc8646fa3aa27e19d52efb082babafc984411c4
Champson8 <eha123lha456@gmail.com>
2026-04-16 09:28:07 -0700

    add output_writing.py with writing matrix to file functionality

---
+++ app_io/output_writing.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from time import sleep
+from matrix import Matrix
+from constants import HIDE_CURSOR, SHOW_CURSOR, EXIT_CONTROLS
+from utils import clearConsole, printTitle, getUserAction
+
+
+def _showError(message: str):
+    print(HIDE_CURSOR)
+    print(message)
+    sleep(3)
+
+
+def writeMatrixToFile(matrix: Matrix):
+    isPathValid = False
+    while not isPathValid:
+        print(SHOW_CURSOR)
+        clearConsole()
+        printTitle("guardar matriz")
+
+        print(EXIT_CONTROLS)
+        path = Path(input("Directorio del archivo .txt: ")).resolve()
+
+        if path == "exit":
+            print(HIDE_CURSOR)
+            return None
+        elif path.exists():
+            while True:
+                print(f"{path.name} ya existe.")
+                print("* ENTER para sobreescribir | ESC para regresar")
+                action = getUserAction()
+                if action == "ESCAPE":
+                    return None
+                elif action == "ENTER":
+                    break
+
+        try:
+            if path.suffix != ".txt":
+                raise ValueError('La extensión del archivo debe ser ".txt".')
+            path.parent.mkdir(parents=True, exist_ok=True)
+        except Exception as error:
+            _showError(f"No se pudo crear el archivo: {error}")
+            return None
+
+        try:
+            lines = list(map(lambda row: " ".join(map(str, row)) + "\n", matrix.rows))
+            with open(path, "w", encoding="utf-8") as file:
+                file.writelines(lines)
+            return path
+        except PermissionError:
+            _showError(
+                "No se tiene permisos suficientes para guardar en este directorio."
+            )
+            return None
+        except Exception as error:
+            _showError(f"No se pudo crear el archivo: {error}")
